3671b9574cfc051f02dd0a0ca5f4990e2d4a3e2f
Clerc Tom <tom.clerc@epitech.eu>
2025-04-02 10:17:16 +0200

    [ADD] : BETTER SENDING MESSAGE

--- graph.py
+++ graph.py
@@ -7,25 +7,27 @@ temperatures = []
 humidites = []
 lumieres = []
 
-with open("donnees.csv", "r") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        temperatures.append(int(row["temperature"]))
-        humidites.append(int(row["humidite"]))
-        lumieres.append(int(row["luminosite"]))
-        dates.append(datetime.strptime(row["datetime"], "%Y-%m-%d %H:%M:%S"))
+def genere_graph_pdf():
+    with open("donnees.csv", "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            temperatures.append(int(row["temperature"]))
+            humidites.append(int(row["humidite"]))
+            lumieres.append(int(row["luminosite"]))
+            dates.append(datetime.strptime(row["datetime"], "%Y-%m-%d %H:%M:%S"))
 
-plt.figure(figsize=(12, 6))
+    plt.figure(figsize=(12, 6))
 
-plt.plot(dates, temperatures, label="Température (°C)", marker="o")
-plt.plot(dates, humidites, label="Humidité", marker="s")
-plt.plot(dates, lumieres, label="Luminosité", marker="^")
+    plt.plot(dates, temperatures, label="Température (°C)", marker="o")
+    plt.plot(dates, humidites, label="Humidité", marker="s")
+    plt.plot(dates, lumieres, label="Luminosité", marker="^")
 
-plt.title("📊 Suivi environnemental complet")
-plt.xlabel("Date & Heure")
-plt.ylabel("Valeurs")
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
+    plt.title("📊 Suivi environnemental complet")
+    plt.xlabel("Date & Heure")
+    plt.ylabel("Valeurs")
+    plt.xticks(rotation=45)
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("graphe.pdf")
+    plt.close()
---
+++ send_bot.py
@@ -0,0 +1,28 @@
+import requests
+from datetime import datetime
+import time
+from graph import genere_graph_pdf
+
+TOKEN = "YOUR_TOKKEN_TELEGRAM"
+TOKEN_BOT = "BOT_TOKKEN"
+
+horaire_actuel = datetime.now().strftime("%H:%M:%S")
+horaire_needed = ("08:00:00")
+
+def envoyer_pdf_telegram(token_bot, chat_id, chemin_fichier):
+    url = f"https://api.telegram.org/bot{token_bot}/sendDocument"
+    with open(chemin_fichier, "rb") as fichier:
+        data = {"chat_id": chat_id}
+        files = {"document": fichier}
+        response = requests.post(url, data=data, files=files)
+    return response
+
+while True:
+    heure = datetime.now().strftime("%H:%M:%S")
+    if heure == "08:00:00":
+        genere_graph_pdf()
+        envoyer_pdf_telegram(TOKEN_BOT, TOKEN, "graphe.pdf")
+        print("✅ Fichier envoyé !")
+        time.sleep(61)
+    else:
+        time.sleep(1)
